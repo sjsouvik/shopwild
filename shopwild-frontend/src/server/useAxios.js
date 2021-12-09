d86fc6a779439c8ee4663dcf608b4e6ca2bbb1a6
Souvik Jana <souvikjana2013@gmail.com>
2021-12-09 16:21:51 +0530

    fix: logout user if error 401 is encountered when fetching cart or wishlist details

--- shopwild-frontend/src/server/useAxios.js
+++ shopwild-frontend/src/server/useAxios.js
@@ -9,52 +9,58 @@ const useAxios = (endpoint, propertyToInitialize) => {
   const [error, setError] = useState(false);
 
   const { dispatch } = useData();
-  const { authUser, authToken } = useAuth();
+  const { authUser, authToken, logout } = useAuth();
 
-  useEffect(() => {
-    (async () => {
-      setLoading(true);
+  const getProducts = async () => {
+    setLoading(true);
 
-      if (endpoint === "product") {
-        const { response, error } = await serverRequests({
-          requestType: "get",
-          url: `${process.env.REACT_APP_BACKEND}/${endpoint}`,
+    if (endpoint === "product") {
+      const { response, error } = await serverRequests({
+        requestType: "get",
+        url: `${process.env.REACT_APP_BACKEND}/${endpoint}`,
+      });
+
+      if (!error) {
+        dispatch({
+          type: "INITIALIZE_DATA",
+          payload: {
+            name: propertyToInitialize,
+            data: response.data[endpoint],
+          },
         });
-
-        if (!error) {
-          dispatch({
-            type: "INITIALIZE_DATA",
-            payload: {
-              name: propertyToInitialize,
-              data: response.data[endpoint],
-            },
-          });
-        }
-
-        if (error) {
-          setError(true);
-        }
-      } else if (authToken) {
-        const { response, error } = await serverRequests({
-          requestType: "get",
-          url: `${process.env.REACT_APP_BACKEND}/${endpoint}/${authUser?._id}`,
-          token: { headers: { authorization: `Bearer ${authToken}` } },
-        });
-
-        if (!error) {
-          dispatch({
-            type: "INITIALIZE_DATA",
-            payload: { name: endpoint, data: response.data[endpoint] },
-          });
-        }
-
-        if (error) {
-          setError(true);
-        }
       }
 
-      setLoading(false);
-    })();
+      if (error) {
+        setError(true);
+      }
+    } else if (authToken) {
+      const { response, error, statusCode } = await serverRequests({
+        requestType: "get",
+        url: `${process.env.REACT_APP_BACKEND}/${endpoint}/${authUser?._id}`,
+        token: { headers: { authorization: `Bearer ${authToken}` } },
+      });
+
+      if (!error) {
+        dispatch({
+          type: "INITIALIZE_DATA",
+          payload: { name: endpoint, data: response.data[endpoint] },
+        });
+      }
+
+      if(statusCode === 401){
+        return logout();
+      }
+
+      if (error) {
+        setError(true);
+      }
+    }
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getProducts();
   }, []);
 
   return [loading, error];
