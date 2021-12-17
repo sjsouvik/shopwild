728c7ad454dfbfda004dbe70aeca58455129a4a7
Souvik Jana <souvikjana2013@gmail.com>
2021-12-17 12:43:12 +0530

    refactor: updated loader

--- shopwild-frontend/src/components/Loader/Loader.jsx
+++ shopwild-frontend/src/components/Loader/Loader.jsx
@@ -1,7 +1,12 @@
 import "./Loader.css";
 
 const Loader = () => {
-  return <div className="loader" data-testid="productLoader"></div>;
+  return (
+    <div className="loader-wrapper">
+      <div className="loader" data-testid="productLoader"></div>
+      <div className="loading-text">Loading...</div>
+    </div>
+  );
 };
 
 export default Loader;
