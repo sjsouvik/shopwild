dd5123d704b48c6efd44dd2f615e839fcb5ae5a1
Souvik Jana <souvikjana2013@gmail.com>
2023-03-19 17:16:56 +0530

    test: update queries to select elements in test of cart page

--- shopwild-frontend/src/integrationTest/cart.integration.test.js
+++ shopwild-frontend/src/integrationTest/cart.integration.test.js
@@ -64,36 +64,40 @@ describe("Integration test for cart", () => {
   test("should render all products, add some products to cart and remove product from cart", async () => {
     await act(async () => render(<App />));
 
-    const cartBtns = screen.getAllByTestId("cartButton");
+    const cartBtns = screen.getAllByRole("button", { name: /add to cart/i });
     await act(async () => fireEvent.click(cartBtns[0]));
     const toastMessage = screen.getByText(/added to cart!/i);
-    expect(toastMessage).toBeDefined();
+    expect(toastMessage).toBeInTheDocument();
 
-    const cartLink = screen.getByTestId("cartLink");
+    const cartLink = screen.getByRole("link", { name: /cart/i });
     await act(async () => fireEvent.click(cartLink));
     expect(window.location.href).toContain("cart");
 
     const cartProducts = screen.getAllByTestId("cartProduct");
     expect(cartProducts.length).toBe(1);
 
-    const removeFromCartBtns = screen.getAllByTestId("removeButton");
+    const removeFromCartBtns = screen.getAllByRole("button", {
+      name: /remove/i,
+    });
     await act(async () => fireEvent.click(removeFromCartBtns[0]));
     expect(screen.queryAllByTestId("cartProduct").length).toBe(0);
   });
 
-  test("should move the product to wishlist on click of move to wishlist button", async () => {
+  test("should move the product from cart to wishlist on click of the move to wishlist button", async () => {
     await act(async () => render(<App />));
 
     const allProductsLink = screen.getByTestId("allProductsLink");
     await act(async () => fireEvent.click(allProductsLink));
-    const cartBtns = screen.getAllByTestId("cartButton");
+    const cartBtns = screen.getAllByRole("button", { name: /add to cart/i });
     await act(async () => fireEvent.click(cartBtns[0]));
     await act(async () => fireEvent.click(cartBtns[0]));
 
-    const moveToWishlistBtns = screen.getAllByTestId("moveToWishlistButton");
+    const moveToWishlistBtns = screen.getAllByRole("button", {
+      name: /move to wishlist/i,
+    });
     await act(async () => fireEvent.click(moveToWishlistBtns[0]));
 
-    const wishlistLink = screen.getByTestId("wishlistLink");
+    const wishlistLink = screen.getByRole("link", { name: /wishlist/i });
     await act(async () => fireEvent.click(wishlistLink));
 
     const wishlistedProducts = screen.getAllByTestId("productCard");
