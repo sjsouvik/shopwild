51a09b12d636ffc3cc56021ba696b748ec061de3
Souvik Jana <souvikjana2013@gmail.com>
2023-03-19 18:07:27 +0530

    test: remove mock of axios from tests of cart page

--- shopwild-frontend/src/common/endpoints.js
+++ shopwild-frontend/src/common/endpoints.js
@@ -2,4 +2,6 @@ const baseUrl = process.env.REACT_APP_BACKEND;
 
 export const endpoints = {
   products: `${baseUrl}/product`,
+  wishlist: `${baseUrl}/wishlist/:userId`,
+  cart: `${baseUrl}/cart/:userId`,
 };
--- shopwild-frontend/src/mocks/handlers.js
+++ shopwild-frontend/src/mocks/handlers.js
@@ -7,6 +7,26 @@ export const handlers = [
     return res(ctx.status(200), ctx.json({ product: products }));
   }),
 
+  rest.get(endpoints.wishlist, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ wishlist: [] }));
+  }),
+
+  rest.post(endpoints.wishlist, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+
+  rest.get(endpoints.cart, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ cart: [] }));
+  }),
+
+  rest.post(endpoints.cart, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+
+  rest.delete(endpoints.cart, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+
   rest.get("*", (req, res, ctx) => {
     console.error(`Please add request handler for ${req.url.toString()}`);
     return res(
